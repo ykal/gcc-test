2f8409e63dec816323bac07d02da51de87f4f3e9
Kaleab Yitbarek <kalzbest@gmail.com>
2019-08-27 18:57:31 +0300

    :sparkles: landing page customization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,48 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { ToastOptions } from 'ng2-toasty';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { ToastOptions } from "ng2-toasty";
 
-import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { PreloadAllModules, RouterModule } from '@angular/router';
-import { APP_ROUTES } from './app.routes';
-import { AuthModule } from './Auth/auth.module';
-import { ApiService } from './shared/services/api.service';
-import { HttpModule } from '@angular/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from './shared/shared.module';
-import { AuthGuardService } from './Auth/services/auth-guard.service';
-import { SolveitTeamGuardService } from './Auth/services/solveit-team-guard.service';
-import { SolveitMgmtGuardService } from './Auth/services/solveit-mgmt-guard.service';
-import { WinnerProjectModule } from './winnerProject/winnerProject.module';
-import { MomentModule } from 'angular2-moment';
-import { AlumunniDetailComponent } from './alumunni-detail/alumunni-detail.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { NgxSpinnerModule } from 'ngx-spinner';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgCircleProgressModule } from 'ng-circle-progress';
-import { WeeklyWinnerComponent } from './weeklyWinner/weeklyWinner.component';
-import { FacebookModule } from 'ngx-facebook';
+import { AppComponent } from "./app.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
+import { PreloadAllModules, RouterModule } from "@angular/router";
+import { APP_ROUTES } from "./app.routes";
+import { AuthModule } from "./Auth/auth.module";
+import { ApiService } from "./shared/services/api.service";
+import { HttpModule } from "@angular/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SharedModule } from "./shared/shared.module";
+import { AuthGuardService } from "./Auth/services/auth-guard.service";
+import { SolveitTeamGuardService } from "./Auth/services/solveit-team-guard.service";
+import { SolveitMgmtGuardService } from "./Auth/services/solveit-mgmt-guard.service";
+import { WinnerProjectModule } from "./winnerProject/winnerProject.module";
+import { MomentModule } from "angular2-moment";
+import { AlumunniDetailComponent } from "./alumunni-detail/alumunni-detail.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { NgxSpinnerModule } from "ngx-spinner";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgCircleProgressModule } from "ng-circle-progress";
+import { WeeklyWinnerComponent } from "./weeklyWinner/weeklyWinner.component";
+import { FacebookModule } from "ngx-facebook";
+import { OwlModule } from "ngx-owl-carousel";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LandingPageComponent,
-    AlumunniDetailComponent,
-    PageNotFoundComponent,
-    WeeklyWinnerComponent
-  ],
+  declarations: [AppComponent, LandingPageComponent, PageNotFoundComponent],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule.forRoot(APP_ROUTES, {
-    }),
+    RouterModule.forRoot(APP_ROUTES, {}),
     FormsModule,
     ReactiveFormsModule,
     AuthModule,
     SharedModule,
     BrowserAnimationsModule,
-    FacebookModule.forRoot()
+    FacebookModule.forRoot(),
+    OwlModule
   ],
   providers: [
     ApiService,
@@ -53,4 +48,4 @@ import { FacebookModule } from 'ngx-facebook';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -19,6 +19,54 @@ export class LandingPageComponent implements OnInit {
     public http: Http
   ) {}
 
+  bdImages = [
+    "../../assets/img/BD/photo_2019-08-19_06-08-32.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-09-10.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-09-23.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-09-28.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-09-33.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-09-37.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-09-41.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-09-45.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-09-50.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-09-55.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-10-01.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-10-05.jpg",
+    "../../assets/img/BD/photo_2019-08-19_06-10-09.jpg"
+  ];
+
+  mekeleImages = [
+    "../../assets/img/Mekele/photo_2019-08-19_06-18-32.jpg",
+    "../../assets/img/Mekele/photo_2019-08-19_06-19-01.jpg",
+    "../../assets/img/Mekele/photo_2019-08-19_06-22-32.jpg",
+    "../../assets/img/Mekele/photo_2019-08-19_06-22-41.jpg",
+    "../../assets/img/Mekele/photo_2019-08-19_06-19-24.jpg",
+    "../../assets/img/Mekele/photo_2019-08-19_06-19-16.jpg",
+    "../../assets/img/Mekele/photo_2019-08-19_06-19-08.jpg"
+  ];
+
+  juImages = [
+    "../../assets/img/JU/photo_2019-08-19_06-14-38.jpg",
+    "../../assets/img/JU/photo_2019-08-19_06-15-12.jpg",
+    "../../assets/img/JU/photo_2019-08-19_06-15-17.jpg",
+    "../../assets/img/JU/photo_2019-08-19_06-15-24.jpg"
+  ];
+
+  jacpImages = [
+    "../../assets/img/J.A.C.P/IMG_6303.JPG",
+    "../../assets/img/J.A.C.P/IMG_6305.JPG",
+    "../../assets/img/J.A.C.P/IMG_6306.JPG",
+    "../../assets/img/J.A.C.P/IMG_6308.JPG",
+    "../../assets/img/J.A.C.P/IMG_6310.JPG",
+    "../../assets/img/J.A.C.P/IMG_6311.JPG",
+    "../../assets/img/J.A.C.P/IMG_6312.JPG",
+    "../../assets/img/J.A.C.P/IMG_6313.JPG",
+    "../../assets/img/J.A.C.P/IMG_6314.JPG",
+    "../../assets/img/J.A.C.P/IMG_6315.JPG"
+  ];
+
+  mySlideOptions = { items: 1, dots: true, nav: false };
+
   ngOnInit() {}
 
   toggleMenu() {
